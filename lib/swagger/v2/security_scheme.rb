6055e3fecfab952fa0ab9d03372a49a69fcd6feb
Nik Wakelin <me@nikwakelin.com>
2015-07-21 13:16:40 -0700

    Stop raising on security scheme validation errors, because we don't care

--- lib/swagger/v2/security_scheme.rb
+++ lib/swagger/v2/security_scheme.rb
@@ -11,14 +11,14 @@ module Swagger
       # According to docs, all except description are required. Schema and samples don't match.
 
       # @!group Fixed Fields
-      required_field :type, String
+      field :type, String
       field :description, String
       field :name, String
       field :in, String
-      required_field :flow, String
-      required_field :authorizationUrl, String
+      field :flow, String
+      field :authorizationUrl, String
       field :tokenUrl, String
-      required_field :scopes, Hash
+      field :scopes, Hash
       # @!endgroup
     end
   end
