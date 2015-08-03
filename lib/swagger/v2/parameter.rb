29a885a4d2237301f67d47c470c0654515c34271
Nik Wakelin <me@nikwakelin.com>
2015-08-03 17:46:26 +0100

    Make name no longer required

--- lib/swagger/v2/parameter.rb
+++ lib/swagger/v2/parameter.rb
@@ -6,7 +6,7 @@ module Swagger
     # @see https://github.com/wordnik/swagger-spec/blob/master/versions/2.0.md#parameterObject Parameter Object
     class Parameter < SwaggerObject
       # @!group Fixed Fields
-      required_field :name, String
+      field :name, String
       # required_field :in, String
       field :in, String
       field :description, String
